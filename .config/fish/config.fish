17486789a93dea7f8c750670a282108c241ef2b6
koji.lin <koji.lin@gmail.com>
2023-04-17 10:04:27 +0900

    add starship check

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,15 @@ if type -q exa
   alias lla "ll -a"
 end
 
-starship init fish | source
+if type -q starship
+  starship init fish | source
+end
+
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
 
 set -gx EDITOR vim
+
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
